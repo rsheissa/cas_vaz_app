d69ff70612c81ff1bf3f2f171f88ca3558506d37
rsheissa <rsheissa@users.noreply.github.com>
2018-01-16 21:21:46 -0600

    Se agrega LoginInfo para mostrar la opción de cerrar sesión

--- FacturaWebSite/Default.aspx.cs
+++ FacturaWebSite/Default.aspx.cs
@@ -10,7 +10,6 @@ public partial class _Default : System.Web.UI.Page
 {
   protected void Page_Load(object sender, EventArgs e)
   {
-
   }
 
   protected void LoginControl_Authenticate(object sender, AuthenticateEventArgs e)
--- FacturaWebSite/Site.master.cs
+++ FacturaWebSite/Site.master.cs
@@ -9,6 +9,6 @@ public partial class Site : System.Web.UI.MasterPage
 {
   protected void Page_Load(object sender, EventArgs e)
   {
-
+    LoginInfo.Visible = false;
   }
 }
